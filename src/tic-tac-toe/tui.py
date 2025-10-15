5ca08dd83536aae6d628a848f4f0e24281cbc019
Griger5 <griger04@gmail.com>
2025-10-15 09:14:34 +0200

    add: movable cursor

--- src/tic-tac-toe/tui.py
+++ src/tic-tac-toe/tui.py
@@ -4,10 +4,17 @@ def main(stdscr):
     stdscr.clear()
 
     running = True
+
+    cursor_y = 1
+    cursor_x = 2
+    cursor_step_x = 6
+    cursor_step_y = 4
+
     while running:
         begin_x = 0; begin_y = 0
         height = 50; width = 50
         win = curses.newwin(height, width, begin_y, begin_x)
+        win.clear()
 
         win.addstr("     #     #     \n")
         win.addstr("     #     #     \n")
@@ -21,8 +28,22 @@ def main(stdscr):
         win.addstr("     #     #     \n")
         win.addstr("     #     #     \n")
 
+        char = stdscr.getch()
+        if char == curses.KEY_RIGHT:
+            if cursor_x + cursor_step_x < 15:
+                cursor_x += cursor_step_x
+        elif char == curses.KEY_LEFT: 
+            if cursor_x - cursor_step_x > 0:
+                cursor_x -= cursor_step_x
+        elif char == curses.KEY_UP: 
+            if cursor_y - cursor_step_y > 0:
+                cursor_y -= cursor_step_y
+        elif char == curses.KEY_DOWN:
+            if cursor_y + cursor_step_y < 10:
+                cursor_y += cursor_step_y
+        
+        win.addstr(cursor_y, cursor_x, "X")
         win.refresh()
-        stdscr.getkey()
 
 if __name__ == "__main__":
     curses.wrapper(main)
