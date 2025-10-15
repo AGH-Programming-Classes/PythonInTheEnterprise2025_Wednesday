ae325f32e6bbdabe7a12c7489d20c91cd9deef54
PatrykAdamski2 <patrykadamsk@student.agh.edu.pl>
2025-10-15 09:27:52 +0200

    chore

--- src/hello_world_project/calculator_main.py
+++ src/hello_world_project/calculator_main.py
@@ -2,10 +2,8 @@ def calculator_main():
     running = True
     calculation = -1
     while running:
-         calculation = input("Choose calculation type 0 for addition 1 for subtraction other for exit")
-         if calculation == 0:
-            continue
-         elif calculation == 1:
-            continue
-         else:
+         calculation = input("-1 for exit\n")
+         if calculation == "-1":
             running = False
+            break
+         print(eval(calculation))
