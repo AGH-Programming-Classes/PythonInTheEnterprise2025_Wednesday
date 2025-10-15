919e33f009de168ee049cec9422f601dc3324c5c
Przemek Bukala <bukalaprzemyslaw@gmail.com>
2025-10-15 08:53:34 +0200

    Substract operator

--- src/hello_world_project/example_module.py
+++ src/hello_world_project/example_module.py
@@ -1,2 +1,13 @@
 def add_numbers(a: int, b: int) -> int:
     return a + b
+'''
+Substract operator
+'''
+def substract() -> int:
+    a=input()
+    b=input()
+    print(f'{a}-{b}')
+    return int(a) - int(b)
+
+print(substract())
+
