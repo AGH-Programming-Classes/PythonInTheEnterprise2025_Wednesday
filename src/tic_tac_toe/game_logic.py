ef67800cb93d382be94c05a660cbf32cd302c68e
filbet4 <filbet427@gmail.com>
2025-10-15 09:25:42 +0200

    created game class with get_board method

---
+++ src/tic_tac_toe/game_logic.py
@@ -0,0 +1,7 @@
+class Game:
+    def __init__(self):
+        self.board_length = 3
+        self._board = [[' '] * self.board_length]* self.board_length
+        
+    def get_board():
+        return self._board
