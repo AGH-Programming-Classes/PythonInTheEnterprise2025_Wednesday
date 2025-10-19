cc3a89cc7e06fb5d9ec996fee162c4176eb37c60
bartek <bartamalio@gmail.com>
2025-10-19 19:30:57 +0200

    prosta pogoda

--- src/hello_world_project/main.py
+++ src/hello_world_project/main.py
@@ -1,15 +1,21 @@
-from hello_world_project.utils import greet_user
-from hello_world_project.example_module import add_numbers
+
+cities=[("Krakow",30),("Warszawa",25),("Gdansk",20)]
+
+def weather(city_name):
+    if not city_name:
+        return "Podaj miasto!"
+    for name, temp in cities:
+        if name.lower() == city_name.lower():
+            return f"In {name} is {temp} C "
+    return f"Unknown city ({city_name})"
+
 
 def main():
-    print("=== Hello World Project ===")
+    print("=== Wheather ===")
 
-    name = input("Enter your name: ")
-    greeting = greet_user(name)
-    print(greeting)
-
-    result = add_numbers(10, 5)
-    print(f"The result of adding 10 and 5 is {result}.")
+    city = input("Enter city: ")
+    temperature = weather(city)
+    print(temperature)
 
 if __name__ == "__main__":
     main()
