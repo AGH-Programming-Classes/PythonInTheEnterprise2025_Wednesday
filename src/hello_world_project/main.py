740bd05cc0fe353c80958bc93a51f0b69ebfc64d
bartek <bartamalio@gmail.com>
2025-10-19 22:40:16 +0200

    dodane api pogodowe oraz api zbierajace koordynaty miasta aby pokazywac aktualna pogode w miastach na swiecie

--- src/hello_world_project/main.py
+++ src/hello_world_project/main.py
@@ -1,17 +1,53 @@
+import requests
 
-cities=[("Krakow",30),("Warszawa",25),("Gdansk",20)]
+def get_coordinates(city_name):
+    url = "https://nominatim.openstreetmap.org/search" #uzywamy darmowego geokodera co zamienia nazwe miasta na jego koordynaty
+    
+    params = {
+        "q": city_name,
+        "format": "json" #format w ktorym chcemy uzyskac dane
+    }
+
+    try:
+        response = requests.get(url, params = params,headers={"User-Agent":"WeatherApp"}) #url, parametry, oraz naglowek (kto wysyla zadanie)
+        data = response.json() #data to lista słowników w formacie json
+
+        lat = float(data[0]["lat"]) #konwerujemy dlugosc i szerokosc miasta na floata z pierwszego wyniku zwroconego w data
+        lon = float(data[0]["lon"])
+
+        return lat, lon
+    
+    except requests.RequestException:
+        return None
 
 def weather(city_name):
     if not city_name:
         return "Podaj miasto!"
-    for name, temp in cities:
-        if name.lower() == city_name.lower():
-            return f"In {name} is {temp} C "
-    return f"Unknown city ({city_name})"
+
+    coordinates = get_coordinates(city_name)
+
+    lat, lon = coordinates
+
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    params = {
+        "latitude": lat,
+        "longitude":lon,
+        "current_weather": True #chcemy aktualna pogode wiec ustawiamy ja na true
+    }
+
+    try:
+        response = requests.get(url,params=params)
+        data = response.json()
+        temp = data["current_weather"]["temperature"]
+        return f"W {city_name} jest {temp} C."
+    
+    except requests.RequestException:
+        return f"Blad polaczenia"
 
 
 def main():
-    print("=== Wheather ===")
+    print("=== Weather ===")
 
     city = input("Enter city: ")
     temperature = weather(city)
