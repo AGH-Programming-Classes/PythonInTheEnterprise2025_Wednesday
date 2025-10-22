5b529b4b4044c58a7ef239411a10f8b1cb07746e
Przemek Bukala <bukalaprzemyslaw@gmail.com>
2025-10-22 09:09:28 +0200

    Add Timer Decorator

--- src/hello_world_project/calculator.py
+++ src/hello_world_project/calculator.py
@@ -5,29 +5,38 @@ class TimerDecorator:
     def __call__(self, *args, **kwargs):
         print("Starting calculator  operations")
         start_time = time.time()
-        result = self.func(*args, **kwargs)
+        result = self.func(self,*args)
         end_time = time.time()
         print(f"Function {self.func.__name__} executed in {end_time - start_time} seconds")
         return result
 
+class Calculator:
+    def __init__(self):
+        pass
+    @TimerDecorator
+    def add(self,a,b):
+        return a+b
+    @TimerDecorator
+    def subtract(self,a,b):
+        return a-b
+    @TimerDecorator
+    def multiply(self,a,b):
+        return a*b
+    @TimerDecorator
+    def divide(self,a,b):
+        if b==0:
+            raise ArithmeticError("")
+        return a/b
+
+
+
+if __name__=="__main__":
+    calc = Calculator()
+    
+    print(calc.add(1,2))
 
 
 
 
 
 
-
-
-
-# def decorator(fun):
-#     def logger(self):
-#         print("Starting operations")
-
-
-
-@TimerDecorator
-def example_function(n):
-    total = 0
-    for i in range(n):
-        total += i
-    return total
