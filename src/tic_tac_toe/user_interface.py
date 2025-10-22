ec9a196183692df2f98a34e11eb67facad15bcfd
ralphy127 <wybraniecrafal01@gmail.com>
2025-10-22 09:30:48 +0200

    added getMove method in ui (wip)

--- src/tic_tac_toe/user_interface.py
+++ src/tic_tac_toe/user_interface.py
@@ -1,11 +1,11 @@
 from tic_tac_toe.game_logic import Game
 from typing import List
 
-class Printer:
+class BoardDrawer:
     def draw(self, board : List[List[int]]):
         pass
 
-class ConsoleBoardPrinter(Printer):
+class ConsoleBoardDrawer(BoardDrawer):
     def draw(self, board : List[List[int]]):
         print("\t+---+---+---+");
         for x in range(3):
@@ -15,18 +15,31 @@ class ConsoleBoardPrinter(Printer):
             print("|\n\t+---+---+---+")
 
 class UserInterface:   
-    def __init__(self, printer : Printer):
-        self._printer = printer
+    def __init__(self, boardPrinter : BoardDrawer):
+        self._boardPrinter = boardPrinter
 
     def drawBoard(self, board : List[List[int]]):
-        self._printer.draw(board)
+        self._boardPrinter.draw(board)
+
+    def getMove(self, player : str, board : List[List[int]]) -> List[int]:
+        receivedCorrectMove = False
+
+        while not receivedCorrectMove:
+            move = input(f"\tPlayer {player}, make a move (x, y)\n").split(",")
+            if len(move) == 2 and 0 < move[0] < 3 and 0 < move[1] < 3 and board[move[0]][move[1]] == " ":
+                receivedCorrectMove = True
+            else:
+                print("\tIncorrect move, please try again.")
+
+        return move
+
 
 def startMainLoop(game : Game):
     is_running = True
 
-    userInterface = UserInterface(ConsoleBoardPrinter())
+    userInterface = UserInterface(ConsoleBoardDrawer())
 
     while is_running:
         userInterface.drawBoard(game.get_board())
-
+        userInterface.getMove("X", game.board)
         break
