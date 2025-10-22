30f7f253462fba0206c5cca85b8040c185822d48
Hubert <hregec@student.agh.edu.pl>
2025-10-22 08:41:24 +0200

    KH SimpleCalculator Added

--- src/hello_world_project/example_module.py
+++ src/hello_world_project/example_module.py
@@ -1,14 +1,16 @@
-def add_numbers(a: float, b: float) -> float:
-    return a + b
+class SimpleCalculator:
+    def add_numbers(self, a: float, b: float) -> float:
+        return a + b
 
-def sub_numbers(a: float, b:float) -> float:
-    return a - b
+    def sub_numbers(self, a: float, b:float) -> float:
+        return a - b
 
-def multiply_numbers(a: float, b: float) -> float:
-    return a * b
+    def multiply_numbers(self, a: float, b: float) -> float:
+        return a * b
 
-def div_numbers(a: float, b: float) -> float:
-    if(abs(b)<0.0001):
-        return a / b
-    else:
-        raise Exception
+    def div_numbers(self, a: float, b: float) -> float:
+        if(abs(b)<0.0001):
+            return a / b
+        else:
+            raise Exception
+
