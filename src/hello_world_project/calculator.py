35f7b5ecd759a1032b1d05efdea407bcc0fd00f2
Przemek Bukala <bukalaprzemyslaw@gmail.com>
2025-10-22 09:23:43 +0200

    Change Calculator into  Singleton

--- src/hello_world_project/calculator.py
+++ src/hello_world_project/calculator.py
@@ -10,7 +10,17 @@ class TimerDecorator:
         print(f"Function {self.func.__name__} executed in {end_time - start_time} seconds")
         return result
 
-class Calculator:
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class Calculator(metaclass=SingletonMeta):
     def __init__(self):
         pass
     @TimerDecorator
@@ -32,8 +42,14 @@ class Calculator:
 
 if __name__=="__main__":
     calc = Calculator()
-    
     print(calc.add(1,2))
+    calc_1 = Calculator()
+    calc_2 = Calculator()
+    if id(calc_1) == id(calc_2):
+        print("Singleton works.")
+    else:
+        print("Singleton failed.")
+
 
 
 
